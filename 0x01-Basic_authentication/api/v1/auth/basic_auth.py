c7c5c3aefe851df0ef2c8fbcd119585c1c988cde
Emmanuel Adaja <emmanueladaja@yahoo.com>
2024-01-20 12:14:07 +0000

    Implemented extraction of credentials from decoded code

--- 0x01-Basic_authentication/api/v1/auth/basic_auth.py
+++ 0x01-Basic_authentication/api/v1/auth/basic_auth.py
@@ -30,3 +30,12 @@ class BasicAuth(Auth):
             return decoded_bytes.decode("utf-8")
         except Exception:
             return None
+
+    def extract_user_credentials(self, decoded_base64_authorization_header: str
+                                 ) -> (str, str):
+        """Returns the extracted username and password from decoded string."""
+        if not decoded_base64_authorization_header or\
+                type(decoded_base64_authorization_header) is not str or\
+                ':' not in decoded_base64_authorization_header:
+            return (None, None)
+        return decoded_base64_authorization_header.split(':')
