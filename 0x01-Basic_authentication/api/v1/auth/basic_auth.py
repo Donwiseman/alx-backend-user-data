87785244061b3b8007f9f15eb77a9defa720653c
Emmanuel Adaja <emmanueladaja@yahoo.com>
2024-01-20 04:45:48 +0000

    Added extraction of Base64 encoding from auth header

--- 0x01-Basic_authentication/api/v1/auth/basic_auth.py
+++ 0x01-Basic_authentication/api/v1/auth/basic_auth.py
@@ -8,4 +8,11 @@ from api.v1.auth.auth import Auth
 
 class BasicAuth(Auth):
     """Implements the basic authentication system for api."""
-    pass
+    def extract_base64_authorization_header(self,
+                                            authorization_header: str) -> str:
+        """Extracts the Base64 enconding string for basic authy."""
+        if not authorization_header or type(authorization_header) is not str:
+            return None
+        if authorization_header.startswith("Basic "):
+            return authorization_header.split()[1]
+        return None
