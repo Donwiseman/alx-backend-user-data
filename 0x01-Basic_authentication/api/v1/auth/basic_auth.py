30105ca7e83e6082269b97d9b268f9d8bd93cffc
Emmanuel Adaja <emmanueladaja@yahoo.com>
2024-01-20 15:44:21 +0000

    Implemented the basic auth

--- 0x01-Basic_authentication/api/v1/auth/basic_auth.py
+++ 0x01-Basic_authentication/api/v1/auth/basic_auth.py
@@ -57,3 +57,24 @@ class BasicAuth(Auth):
             if obj.is_valid_password(user_pwd):
                 return obj
         return None
+
+    def current_user(self, request=None) -> TypeVar('User'):
+        """
+        Returns the current user who sent the request
+        based on authentication.
+        """
+        if request is None:
+            return None
+        auth_header = self.authorization_header(request)
+        if not auth_header:
+            return None
+        encoded_str = self.extract_base64_authorization_header(auth_header)
+        if not encoded_str:
+            return None
+        decoded_str = self.decode_base64_authorization_header(encoded_str)
+        if not decoded_str:
+            return None
+        email, pwd = self.extract_user_credentials(decoded_str)
+        if not email or not pwd:
+            return None
+        return self.user_object_from_credentials(email, pwd)
