c85791c7cead43bf243c78bd9c24c12cd7822eda
Emmanuel Adaja <emmanueladaja@yahoo.com>
2024-01-21 04:17:26 +0000

    Added session id

--- 0x02-Session_authentication/api/v1/auth/session_auth.py
+++ 0x02-Session_authentication/api/v1/auth/session_auth.py
@@ -5,7 +5,16 @@ Session Authentication class for the API
 
 from api.v1.auth.auth import Auth
 from typing import TypeVar
+import uuid
 
 
 class SessionAuth(Auth):
     """ Session Management class for the API. """
+    user_id_by_session_id = {}
+
+    def create_session(self, user_id: str = None) -> str:
+        if not user_id or type(user_id) is not str:
+            return None
+        session_id = str(uuid.uuid4())
+        self.user_id_by_session_id[session_id] = user_id
+        return session_id
