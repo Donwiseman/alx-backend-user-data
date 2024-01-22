6d1880c8ef331a523fa807f25348ff3457ccbc72
Emmanuel Adaja <emmanueladaja@yahoo.com>
2024-01-22 16:37:11 +0000

    Added the DB class for creating connection

--- 0x03-user_authentication_service/user.py
+++ 0x03-user_authentication_service/user.py
@@ -10,8 +10,18 @@ class User(Base):
     """User base model"""
     __tablename__ = 'users'
 
-    id = Column(Integer, primary_key=True)
+    id = Column(Integer, primary_key=True, autoincrement=True)
     email = Column(String(250), nullable=False)
     hashed_password = Column(String(250), nullable=False)
     session_id = Column(String(250))
     reset_token = Column(String(250))
+
+    def __init__(self, email: str, hashed_password: str,
+                 session_id: str = None, reset_token: str = None) -> None:
+        """ initializes the class. """
+        self.email = email
+        self.hashed_password = hashed_password
+        if session_id:
+            self.session_id = session_id
+        if reset_token:
+            self.reset_token = reset_token
