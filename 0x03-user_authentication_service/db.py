edaf14c12c60470db47694f89c316d80cf0a2fa7
Emmanuel Adaja <emmanueladaja@yahoo.com>
2024-01-23 20:49:56 +0000

    Added find user

--- 0x03-user_authentication_service/db.py
+++ 0x03-user_authentication_service/db.py
@@ -5,6 +5,8 @@ from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 from sqlalchemy.orm.session import Session
+from sqlalchemy.exc import InvalidRequestError
+from sqlalchemy.orm.exc import NoResultFound
 
 from user import Base, User
 
@@ -38,3 +40,15 @@ class DB:
         self._session.add(new_user)
         self._session.commit()
         return new_user
+
+    def find_user_by(self, **kwargs: str) -> User:
+        """ Finds User with passed characteristics."""
+        arg_list = ["email", "hashed_password", "session_id", "reset_token"]
+        if kwargs:
+            for key, value in kwargs.items():
+                if key in arg_list:
+                    for user in self._session.query(User).all():
+                        if getattr(user, key) == value:
+                            return user
+                    raise NoResultFound
+            raise InvalidRequestError
