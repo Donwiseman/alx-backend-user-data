c229cf64ff63a95d814abde8fea36df99aedfa9b
Emmanuel Adaja <emmanueladaja@yahoo.com>
2024-01-25 03:40:55 +0000

    Added valid login

--- 0x03-user_authentication_service/auth.py
+++ 0x03-user_authentication_service/auth.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python3
 """ The authentication class for the model."""
-from bcrypt import hashpw, gensalt
+from bcrypt import hashpw, gensalt, checkpw
 from db import DB
 from user import User
 from sqlalchemy.exc import InvalidRequestError
@@ -29,3 +29,12 @@ class Auth:
         except InvalidRequestError or NoResultFound:
             hashed_passwd = _hash_password(password)
             return self._db.add_user(email, hashed_passwd)
+
+    def valid_login(self,  email: str, password: str) -> bool:
+        """Validates a password login."""
+        try:
+            user = self._db.find_user_by(email=email)
+            pw_bytes = password.encode('utf-8')
+            return checkpw(pw_bytes, user.hashed_password)
+        except InvalidRequestError or NoResultFound:
+            return False
