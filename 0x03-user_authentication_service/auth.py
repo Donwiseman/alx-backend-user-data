4164c38b7159c005748ca48053269429aac6eb3d
Emmanuel Adaja <emmanueladaja@yahoo.com>
2024-01-25 19:51:52 +0000

    Added a reset token function

--- 0x03-user_authentication_service/auth.py
+++ 0x03-user_authentication_service/auth.py
@@ -70,3 +70,13 @@ class Auth:
             self._db.update_user(user_id, session_id=None)
         except InvalidRequestError or NoResultFound or ValueError:
             return None
+
+    def get_reset_password_token(self, email: str) -> str:
+        """Creates a reset token for the user."""
+        try:
+            user = self._db.find_user_by(email=email)
+            reset_token = _generate_uuid()
+            self._db.update_user(user.id, reset_token=reset_token)
+            return reset_token
+        except InvalidRequestError or NoResultFound or ValueError:
+            raise ValueError
