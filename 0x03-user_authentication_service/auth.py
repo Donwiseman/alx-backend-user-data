98cc8d47bf35e29c129e599f36b61437645b4767
Emmanuel Adaja <emmanueladaja@yahoo.com>
2024-01-26 02:30:23 +0000

    Update password with reset token

--- 0x03-user_authentication_service/auth.py
+++ 0x03-user_authentication_service/auth.py
@@ -80,3 +80,13 @@ class Auth:
             return reset_token
         except InvalidRequestError or NoResultFound or ValueError:
             raise ValueError
+
+    def update_password(self, reset_token: str, password: str) -> None:
+        """Updates the new password from the passed reset token."""
+        try:
+            user = self._db.find_user_by(reset_token=reset_token)
+            hashed_password = _hash_password(password)
+            self._db.update_user(user.id, hashed_password=hashed_password)
+            self._db.update_user(user.id, reset_token=None)
+        except InvalidRequestError or NoResultFound or ValueError:
+            raise ValueError
